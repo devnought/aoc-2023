b8988b495cd3b774f0def533172c2d873f5c9804
Kyle Gretchev <kgretchev@gmail.com>
2023-12-03 04:52:52 -0500

    Day 03 part 01

---
+++ day03/src/main.rs
@@ -0,0 +1,174 @@
+use nom::{
+    branch::alt,
+    character::complete::{anychar, char, u64},
+    combinator::verify,
+    Finish, IResult,
+};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> anyhow::Result<()> {
+    let res = part01()?;
+    println!("Part 01: {res}");
+
+    Ok(())
+}
+
+fn part01() -> anyhow::Result<u64> {
+    let file = File::open("day03.txt")?;
+    let reader = BufReader::new(file);
+
+    let iter = reader
+        .lines()
+        .map_while(Result::ok)
+        .enumerate()
+        .map(|(y, line_value)| line(y as i64, &line_value));
+
+    let mut values = HashMap::new();
+    let mut symbols = Vec::new();
+
+    for (v, s) in iter {
+        for value in v {
+            let stored_value = StoredValue {
+                id: value.start,
+                value: value.value,
+            };
+
+            values.insert(value.coords(), stored_value);
+        }
+
+        for symbol in s {
+            symbols.push(symbol);
+        }
+    }
+
+    let mut seen_values = HashSet::new();
+    let mut sum = 0;
+
+    for symbol in symbols {
+        for coords in symbol.adjacent_cells() {
+            if let Some(stored_value) = values.get(&coords) {
+                if seen_values.contains(&stored_value.id) {
+                    continue;
+                }
+
+                seen_values.insert(stored_value.id);
+
+                sum += stored_value.value;
+            }
+        }
+    }
+
+    Ok(sum)
+}
+
+#[derive(Debug)]
+struct StoredValue {
+    id: (i64, i64),
+    value: u64,
+}
+
+#[derive(Debug)]
+struct Symbol(char, i64, i64);
+
+impl Symbol {
+    fn adjacent_cells(&self) -> [(i64, i64); 9] {
+        let x = self.1;
+        let y = self.2;
+
+        let mut index = 0;
+        let mut positions = [(0, 0); 9];
+
+        for x_pos in (x - 1)..(x + 2) {
+            for y_pos in (y - 1)..(y + 2) {
+                positions[index] = (x_pos, y_pos);
+                index += 1;
+            }
+        }
+
+        positions
+    }
+}
+
+#[derive(Debug)]
+struct DataValue {
+    start: (i64, i64),
+    value: u64,
+    x: i64,
+    y: i64,
+}
+
+impl DataValue {
+    fn coords(&self) -> (i64, i64) {
+        (self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+enum DataRaw {
+    Blank,
+    Symbol(char),
+    Value(u64),
+}
+
+fn line(y: i64, input: &str) -> (Vec<DataValue>, Vec<Symbol>) {
+    let mut values = Vec::new();
+    let mut symbols = Vec::new();
+
+    let mut x = 0;
+    let mut remaining = input;
+
+    while !remaining.is_empty() {
+        let (r, data) = data(remaining).finish().unwrap();
+        let len = (remaining.len() - r.len()) as i64;
+
+        match data {
+            DataRaw::Blank => {}
+            DataRaw::Symbol(s) => {
+                let value = Symbol(s, x, y);
+                symbols.push(value);
+            }
+            DataRaw::Value(value) => {
+                for new_x in x..(x + len) {
+                    let value = DataValue {
+                        start: (x, y),
+                        value,
+                        x: new_x,
+                        y,
+                    };
+                    values.push(value);
+                }
+            }
+        }
+
+        x += len;
+        remaining = r;
+    }
+
+    (values, symbols)
+}
+
+fn symbol(input: &str) -> IResult<&str, DataRaw> {
+    let (input, s) = verify(anychar, |c| !c.is_alphanumeric() && *c != '.')(input)?;
+    let symbol = DataRaw::Symbol(s);
+    Ok((input, symbol))
+}
+
+fn blank(input: &str) -> IResult<&str, DataRaw> {
+    let (input, _) = char('.')(input)?;
+    let blank = DataRaw::Blank;
+    Ok((input, blank))
+}
+
+fn number(input: &str) -> IResult<&str, DataRaw> {
+    let (remaining, num) = u64(input)?;
+    let value = DataRaw::Value(num);
+    Ok((remaining, value))
+}
+
+fn data(input: &str) -> IResult<&str, DataRaw> {
+    alt((blank, symbol, number))(input)
+}
